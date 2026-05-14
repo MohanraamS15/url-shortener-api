c0fa21fe70bd0c29e9ba330d204215d4178f7107
MohanraamS15 <mohanraams15@gmail.com>
2026-05-14 15:09:47 +0530

    Added security middleware and rate limiting

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 require('dotenv').config();
 
+const helmet = require('helmet');
+const cors = require('cors');
+const rateLimiter = require('express-rate-limit');
+
 const express=require('express');
 const app=express();
 
@@ -10,19 +14,37 @@ require('./cron/deleteInactiveURL');
 const notFound=require('./middleware/notFoundMiddleware');
 const errorHandler=require('./middleware/errorHandlerMiddleware');
 
+
+app.use(helmet());
+app.use(cors());
+app.use(
+    rateLimiter({
+        windowMs:15*60*1000,
+        max:1,
+        message:'Too many requests from this IP'
+    })
+);
+
 app.use(express.json());
+app.use(express.urlencoded({extended:true}));
 
 app.use('/shorten',urlRouter);
 
 app.use(notFound)
 app.use(errorHandler)
 
-port=process.env.PORT || 5000
+const port=process.env.PORT || 5000
 const start=async ()=>{
-    await connectDB(process.env.MONGO_URI);
-    app.listen(port,()=>{
-        console.log('Server is running....');  
-    })
+    try{
+        await connectDB(process.env.MONGO_URI);
+        app.listen(port,()=>{
+            console.log('Server is running....');  
+        })
+    }
+    catch(error){
+        console.log(error);
+    }
+    
 
 }
 
